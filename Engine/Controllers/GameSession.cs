b4ba77fec722aa6376691957c6df3d053b4b7f5c
Brooke Bullek <btb004@bucknell.edu>
2018-09-24 13:34:37 -0400

    Created Controller GameSession class to instantiate player

---
+++ Engine/Controllers/GameSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Controllers
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        // Constructor
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Princess Carolyn";
+            CurrentPlayer.Gold = 999999;
+        }
+    }
+}
